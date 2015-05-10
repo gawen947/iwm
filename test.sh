03fa9412d48df95a1bb34bcea5f9e507eb546d77
Rui Paulo <rpaulo@felyko.com>
2015-05-10 13:20:19 -0700

    Add wpa_supplicant to test.sh

--- test.sh
+++ test.sh
@@ -9,6 +9,7 @@ sudo kldload iwmfw/iwm-7265/iwm-7265-9.ko
 set -e
 sudo kldload driver/if_iwm.ko
 sudo wlandebug -d 0xffffffff
-sudo ifconfig wlan1 create wlandev iwm0
+sudo ifconfig wlan0 create wlandev iwm0
 sudo wlandebug -d 0
-sudo ifconfig wlan1 up channel 2 scan
+sudo ifconfig wlan0 up channel 5 
+sudo wpa_supplicant -c /etc/wpa_supplicant.conf -i wlan0 -B
